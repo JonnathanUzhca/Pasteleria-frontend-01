bd659e380d60273df10497f9a4c57ff3e9e3e14f
Jonnathan <jonathan.uzhca0106@gmail.com>
2024-04-28 22:45:44 -0500

    modidificacion de bracum

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -6,11 +6,11 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { SharedModule } from '../shared/shared.module';
 import { BodyComponent } from './pages/body/body.component';
-import { ClienteComponent } from './pages/user/pages/cliente/cliente.component';
-import { StoresComponent } from './pages/stores/stores.component';
-import { ProductComponent } from './pages/product/product.component';
-import { InvoicingComponent } from './pages/invoicing/invoicing.component';
-import { PersonalComponent } from './pages/user/pages/personal/personal.component';
+// import { ClienteComponent } from './pages/user/pages/cliente/cliente.component';
+// import { StoresComponent } from './pages/stores/stores.component';
+// import { ProductComponent } from './pages/product/product.component';
+// import { InvoicingComponent } from './pages/invoicing/invoicing.component';
+// import { PersonalComponent } from './pages/user/pages/personal/personal.component';
 import { ComponentsModule } from './pages/user/components/components.module';
 
 
@@ -18,11 +18,11 @@ import { ComponentsModule } from './pages/user/components/components.module';
   declarations: [
     AdminComponent,
     BodyComponent,
-    ClienteComponent,
-    PersonalComponent,
-    StoresComponent,
-    ProductComponent,
-    InvoicingComponent,
+    // ClienteComponent,
+    // PersonalComponent,
+    // StoresComponent,
+    // ProductComponent,
+    // InvoicingComponent,
 
   ],
   imports: [
