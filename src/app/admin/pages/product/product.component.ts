58fafcd2803faa6b6b519a427c1f13d3f7a732b4
Jonnathan <jonathan.uzhca0106@gmail.com>
2024-06-28 01:31:34 -0500

    version 01.0.0  retomada  modificacion de  css y modificacion de estilos

--- src/app/admin/pages/product/product.component.ts
+++ src/app/admin/pages/product/product.component.ts
@@ -2,32 +2,62 @@ import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 interface ShopType {
   id?: number;
-  badgeStatus: boolean;
-  badge?: string;
-  badgeText?: string;
-  img: string;
+ 
   title: string;
   oldValue: string;
   newValue: string;
-  description: string;
-  rating: number;
-  ratingValue: number;
+  
 }
 
 const ShopData: ShopType[] = [
-  {id: 1, badgeStatus: true, badge: 'success', badgeText: 'In Stock', img:'./assets/img/pngs/01.png', title:"Men's Shoes", oldValue: '$49.00', newValue: '$39.00', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor ipsum quia.', rating: 4 , ratingValue: 25},
-  {id: 2, badgeStatus: false, badge: 'info', badgeText: '-30%', img:'./assets/img/pngs/02.png', title:"Hand Bag", oldValue: '$30.00', newValue: '$21.00', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor ipsum quia.', rating: 4 , ratingValue: 14},
-  {id: 3, badgeStatus: true, badge: 'danger', badgeText: 'No Stock', img:'./assets/img/pngs/03.png', title:"Wrist Watch", oldValue: '$29.00', newValue: '$15.00', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor ipsum quia.', rating: 4 , ratingValue: 22},
-  {id: 4, badgeStatus: false, badge: 'success', badgeText: '-50%', img:'./assets/img/pngs/04.png', title:"Long Frock", oldValue: '$32.00', newValue: '$22.00', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor ipsum quia.', rating: 4 , ratingValue: 20},
-  {id: 5, badgeStatus: false, badge: 'warning', badgeText: '-20%', img:'./assets/img/pngs/05.png', title:"Girls Sandals", oldValue: '$30.00', newValue: '$21.00', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor ipsum quia.', rating: 4 , ratingValue: 14},
-  {id: 6, badgeStatus: true, badge: 'secondary', badgeText: 'New', img:'./assets/img/pngs/06.png', title:"Sofa Chair", oldValue: '$29.00', newValue: '$15.00', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor ipsum quia.', rating: 4 , ratingValue: 22},
-  {id: 7, badgeStatus: false, badge: 'primary', badgeText: '-10%', img:'./assets/img/pngs/07.png', title:"Laptop", oldValue: '$200.00', newValue: '$149.00', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor ipsum quia.', rating: 4 , ratingValue: 19},
-  {id: 8, badgeStatus: true, badge: 'info', badgeText: 'Best Deal', img:'./assets/img/pngs/08.png', title:"Soft Toy", oldValue: '$49.00', newValue: '$39.00', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor ipsum quia.', rating: 3 , ratingValue: 25},
-  {id: 9, badgeStatus: true, badge: 'success', badgeText: 'In Stock', img:'./assets/img/pngs/09.png', title:"Smart Phone", oldValue: '$49.00', newValue: '$39.00', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor ipsum quia.', rating: 4 , ratingValue: 14},
-  {id: 10, badgeStatus: false, badge: 'info', badgeText: '-30%', img:'./assets/img/pngs/010.png', title:"Girls Skirt", oldValue: '$30.00', newValue: '$21.00', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor ipsum quia.', rating: 3 , ratingValue: 25},
-  {id: 11, badgeStatus: true, badge: 'success', badgeText: 'In Stock', img:'./assets/img/pngs/011.png', title:"Fashion Ring", oldValue: '$29.00', newValue: '$15.00', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor ipsum quia.', rating: 3 , ratingValue: 22},
-  {id: 12, badgeStatus: false, badge: 'secondary', badgeText: '-50%', img:'./assets/img/pngs/012.png', title:"Sunglasses", oldValue: '$32.00', newValue: '$22.00', description:'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor ipsum quia.', rating: 3 , ratingValue: 32},
+  {id: 1, title:"Cono Simple", oldValue: '$49.00', newValue: '$39.00'},
+  {id: 2, title:"Cono con Crema", oldValue: '$30.00', newValue: '$21.00'},
+  {id: 3, title:"Cono Doble", oldValue: '$29.00', newValue: '$15.00'},
+  {id: 4, title:"Vaso Simple", oldValue: '$32.00', newValue: '$22.00'},
+  {id: 5, title:"Vaso con Crema", oldValue: '$30.00', newValue: '$21.00'},
+  {id: 6, title:"Vaso Doble", oldValue: '$29.00', newValue: '$15.00'},
+  {id: 7, title:"Tulipan con Crema", oldValue: '$200.00', newValue: '$149.00'},
+  {id: 8, title:"Tulipan Doble", oldValue: '$49.00', newValue: '$39.00'},
+  {id: 9, title:"1L de Helado", oldValue: '$49.00', newValue: '$39.00'},
+  {id: 10, title:"1/2L de Helado", oldValue: '$30.00', newValue: '$21.00'},
+  {id: 11, title:"Barquillo", oldValue: '$29.00', newValue: '$15.00'},
+  {id: 12, title:"Copa Dulce Amor", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 13, title:"Copa Gusano", oldValue: '$30.00', newValue: '$21.00'},
+  {id: 14, title:"Copa Pinocho", oldValue: '$29.00', newValue: '$15.00'},
+  {id: 15, title:"Copa Loca", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 16, title:"Copa Raton", oldValue: '$30.00', newValue: '$21.00'},
+  {id: 17, title:"Banana Split", oldValue: '$29.00', newValue: '$15.00'},
+  {id: 18, title:"Milkshake", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 19, title:"Durazno con Helado", oldValue: '$30.00', newValue: '$21.00'},
+  {id: 20, title:"Helado Frito", oldValue: '$29.00', newValue: '$15.00'},
+  {id: 21, title:"Helado Spaguetti", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 22, title:"Porc B. Chocolate", oldValue: '$30.00', newValue: '$21.00'},
+  {id: 23, title:"Porc B. Mora", oldValue: '$29.00', newValue: '$15.00'},
+  {id: 24, title:"Porc B. Durazno", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 25, title:"Porc B. ChocoMora", oldValue: '$29.00', newValue: '$15.00'},
+  {id: 26, title:"Porc Braunie", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 27, title:"Braunie Helado", oldValue: '$30.00', newValue: '$21.00'},
+  {id: 28, title:"Chocolate Helado", oldValue: '$29.00', newValue: '$15.00'},
+  {id: 29, title:"Mora Helada", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 30, title:"Durazno Helado", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 31, title:"ChocoMora Helado", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 32, title:"Mousse de Maracuya", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 33, title:"Frutos Rojos", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 34, title:"Tiramisu", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 35, title:"Cake", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 36, title:"Torta 10 Pers.", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 37, title:"Torta 15 Pers.", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 38, title:"Torta 20 Pers.", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 39, title:"Brazo Gitano Chocolate", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 40, title:"Brazo Gitano Mora", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 41, title:"Brazo Gitano Nuez", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 42, title:"Brazo Gitano ChocoMora", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 43, title:"Mousse de Maracuya 10 Pers.", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 44, title:"Frutos Rojos 10 Pers.", oldValue: '$32.00', newValue: '$22.00' },
+  {id: 45, title:"Tiramisu 10 Pers.", oldValue: '$32.00', newValue: '$22.00' },
 ]
+
+
 
 @Component({
   selector: 'app-product',
