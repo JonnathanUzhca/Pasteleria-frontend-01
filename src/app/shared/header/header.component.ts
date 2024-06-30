f5bc5e9cb7ab31127b52dc15b4635b1082ba0586
Jonnathan <jonathan.uzhca0106@gmail.com>
2024-06-29 20:47:21 -0500

    verion 01.1.1 modificacion de css para el header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { NavigationStart, Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { fromEvent } from 'rxjs';
 import { LayoutService } from 'src/app/services/layout.service';
 import { Menu, NavService } from 'src/app/services/nav.service';
 import { SwitcherService } from 'src/app/services/switcher.service';
+
 
 @Component({
   selector: 'app-header',
@@ -16,26 +17,21 @@ export class HeaderComponent implements OnInit {
   public menuItems!: Menu[];
   public items!: Menu[];
   public text!: string;
-  public SearchResultEmpty:boolean = false;
-  
+  public SearchResultEmpty: boolean = false;
+    
 
   constructor(
     private layoutService: LayoutService,
     public SwitcherService: SwitcherService,
     public navServices: NavService,
-   
+
     private router: Router,
     private modalService: NgbModal
-  ){
-
-  }
+  ) {}
 
   ngOnInit(): void {
-
-   
-    
   }
- 
+
   toggleSwitcher() {
     this.SwitcherService.emitChange(true);
   }
@@ -43,8 +39,4 @@ export class HeaderComponent implements OnInit {
   toggleSidebarNotification() {
     this.layoutService.emitSidebarNotifyChange(true);
   }
-
-
-
-
 }
